47d7a06357e7d39fe8f7a172cbeeb7b590532341
iammanishk <iammaanisshhh@gmail.com>
2025-02-22 21:57:51 +0530

    BMI project added

---
+++ projects/BMICalculator/script.js
@@ -0,0 +1,24 @@
+console.log('Hello, World!');
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('.result')
+
+    if(height === '' || height <= 0 || isNaN(height)){
+        result.innerHTML = `Please enter a valid height: ${height}`
+    }else if(weight === '' || weight <= 0 || isNaN(weight)){
+        result.innerHTML = `Please enter a valid weight: ${weight}`
+    }else{
+        let bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        result.innerHTML = `Your BMI is: ${bmi}`
+    }
+
+  
+   
+    
+})
